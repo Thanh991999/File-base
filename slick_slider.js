ee79da9cf0bf55aa2a6750ad84684ab594e0e9ef
shikamaru0102 <daybydaylove@gmail.com>
2022-05-15 20:08:14 +0700

    git push

--- slick_slider.js
+++ slick_slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
-  $('.slider-wrap').slick({
-      slidesToShow: 4,
+  $('.slider__wrapper').slick({
+      slidesToShow: 2,
       slidesToScroll: 1,
       speed: 1000,
       infinite: false,
@@ -37,39 +37,3 @@ $(document).ready(function(){
   
 });
 
-$(document).ready(function(){
-  $('.slider-review__wrap').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      speed: 1000,
-      infinite: false,
-      responsive: [
-          {
-            breakpoint: 1082,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ],
-      prevArrow:"<button type='button' class='slick-prev-review pull-left'><i class='fa-solid fa-arrow-left slick-arrow' aria-hidden='true'></i></button>",
-      nextArrow:"<button type='button' class='slick-next-review pull-right'><i class='fa-solid fa-arrow-right slick-arrow' aria-hidden='true'></i></button>"
-  });
-});
